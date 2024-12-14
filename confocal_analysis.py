eb1abddf9c0dddb7b56f61a44c3b970a2a156860
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2024-12-14 22:49:18 +0100

    first confocal analysis

---
+++ confocal_analysis.py
@@ -0,0 +1,164 @@
+import pandas as pd
+import functions as fn
+import fit_peaks as fp
+import numpy as np
+import os
+import functions2 as fn2
+
+R = 0.05  # m, mirror radius of curvature
+c = 3e8  # speed of light
+l = 50e-3  # cavity length (imposing)
+fsr_freq = c/(2*l)  # (expected) fsr
+
+#####################################################################################################
+# section with file specific values
+
+titles = ['scope_' + str(i) for i in range(15, 26)]
+paths = [f'data_confocal/clean_data/{t}.csv' for t in titles]
+os.makedirs(f"data_confocal/figures/calibration", exist_ok=True)
+os.makedirs(f"data_confocal/figures/simple_plots_piezo", exist_ok=True)
+
+#####################################################################################################
+
+# just use data6 folder
+with open('data_confocal/output.txt', 'a') as file:
+    for path, title in zip(paths, titles):
+        print(f'file: {title}', file=file)
+
+        '''read data'''
+        data = pd.read_csv(path)
+
+        timestamps = data['timestamp'].to_numpy()
+        volt_laser = data['volt_laser'].to_numpy()
+        volt_piezo = data['volt_piezo'].to_numpy()
+
+        '''find free spectral range (in voltage units) and finesse from peaks'''
+
+        # find peaks and widths with just the find_peak function (only for plotting)
+        xpeaks, ypeaks, peak_widths, indices = fn.peaks(
+            volt_piezo, volt_laser, 0.08, 400, True)
+
+        # find peaks and widths fitting lorentzians
+        lor, cov = fp.fit_peaks(volt_piezo, volt_laser, 0.08, 400)
+
+        # reoving unwanted peaks from data
+        mask = ((xpeaks > -8.2) | (ypeaks > 0.6))
+        xpeaks = xpeaks[mask]
+        ypeaks = ypeaks[mask]
+        peak_widths = peak_widths[mask]
+        lor = np.array(lor)
+        cov = np.array(cov)
+        lor = lor[mask]
+        cov = cov[mask]
+        indices = indices[mask]
+
+        x0_list = []
+        A_list = []
+        gamma_list = []
+        dx0 = []
+        dA = []
+        dgamma = []
+
+        for popt, pcov in zip(lor, cov):
+            A_list.append(popt[0])
+            x0_list.append(popt[1])
+            gamma_list.append(popt[2])
+            dA.append(np.sqrt(pcov[0, 0]))
+            dx0.append(np.sqrt(pcov[1, 1]))
+            dgamma.append(np.sqrt(pcov[2, 2]))
+
+        x0_list = np.array(x0_list)
+        A_list = np.array(A_list)
+        gamma_list = np.array(gamma_list)
+        dx0 = np.array(dx0)
+        dA = np.array(dA)
+        dgamma = np.array(dgamma)
+
+        print('Assuming confocality', file=file)
+
+        # Generate expected frequencies
+        expected_freq = np.arange(0, fsr_freq/2 * len(xpeaks), fsr_freq/2)
+
+        # find calibration, and plot the data
+        coeffs1, coeffs2, d_coeffs_1, d_coeffs_2 = fn.plot_fits(x0_list, expected_freq, "Peaks in piezo voltage (V)", "Expected frequency (Hz)",
+                                                                f'data_confocal/figures/calibration/calibration_confocal_{title}.pdf',
+                                                                title='Assuming confocality', save=True)
+
+        print('linear coeffs (m * x + q)', file=file)
+        print(f'm = {coeffs1[0]} +/- {d_coeffs_1[0]} Hz/V', file=file)
+        print(f'q = {coeffs1[1]} +/- {d_coeffs_1[1]} Hz', file=file)
+        print('quadratic coeffs (a * x^2 + b* x + c)', file=file)
+        print(f'a = {coeffs2[0]} +/- {d_coeffs_2[0]} Hz/V^2', file=file)
+        print(f'b = {coeffs2[1]} +/- {d_coeffs_2[1]} Hz/V', file=file)
+        print(f'c = {coeffs2[2]} +/- {d_coeffs_2[2]} Hz', file=file)
+
+        # convert piezo voltages into frequencies
+        calibrated_freqs = coeffs2[0] * volt_piezo ** 2 + \
+            coeffs2[1] * volt_piezo + coeffs2[2]
+
+        fn.scattering(x=volt_piezo, y=volt_laser, x_label='Volt_piezo (V)', y_label='Laser Intensity (V)',
+                      title='Laser Intensity',
+                      file_name=f'data_confocal/figures/simple_plots_piezo/{title}.png', save=True)
+
+        fn.scattering(x=calibrated_freqs, y=volt_laser, x_label='Relative frequency values (GHz)', y_label='Laser Intensity (V)',
+                      title='Laser Intensity (calibrated): assuming confocality',
+                      file_name=f'data_confocal/figures/calibration/data_confocal_{title}.png', save=True)
+
+        print('Without assuming confocality', file=file)
+
+        x0_nonconfoc = x0_list[::2]
+
+        # generate expected frequencies
+        expected_freq = np.arange(0, fsr_freq * len(x0_nonconfoc), fsr_freq)
+
+        # find calibration, and plot the data
+        coeffs1, coeffs2, _, _ = fn.plot_fits(x0_nonconfoc, expected_freq, "Peaks in piezo voltage (V)", "Expected frequency (Hz)",
+                                              f'data_confocal/figures/calibration/calibration_non_confocal_{title}.pdf',
+                                              title='Without assuming confocality', save=True)
+
+        print('linear coeffs (m * x + q)', file=file)
+        print(f'm = {coeffs1[0]} Hz/V', file=file)
+        print(f'q = {coeffs1[1]} Hz', file=file)
+        print('quadratic coeffs (a * x^2 + b* x + c)', file=file)
+        print(f'a = {coeffs2[0]} Hz/V^2', file=file)
+        print(f'b = {coeffs2[1]} Hz/V', file=file)
+        print(f'c = {coeffs2[2]} Hz\n', file=file)
+
+        # convert piezo voltages into frequencies
+        calibrated_freqs_1 = coeffs2[0] * volt_piezo**2 + \
+            coeffs2[1] * volt_piezo + coeffs2[2]
+
+        fn.scattering(x=calibrated_freqs, y=volt_laser, x_label='Relative frequency values (GHz)', y_label='Laser Intensity (V)',
+                      title='Laser Intensity (calibrated): without assuming confocality',
+                      file_name=f'data_confocal/figures/calibration/data_non_confocal_{title}.png', save=True)
+
+        data = {'freq_confoc': calibrated_freqs,
+                'freq_non_confoc': calibrated_freqs_1,
+                'volt_laser': volt_laser}
+        df = pd.DataFrame(data)
+
+        new_file_path = f"data_non_confocal/clean_data/{title}_calib.csv"
+        df.to_csv(new_file_path, index=False)
+        print(f"Data saved to {new_file_path}")
+
+        peak_freq_conf = calibrated_freqs[indices]
+        peak_freq_non_conf = calibrated_freqs_1[indices]
+        time_peaks = timestamps[indices]
+
+        pk = {'indices': indices,
+              'timestamp': time_peaks,
+              'laser_peaks': ypeaks,
+              'piezo_peaks': xpeaks,
+              'freq_conf': peak_freq_conf,
+              'freq_non_confoc': peak_freq_non_conf,
+              'lor_A': A_list,
+              'lor_mean': x0_list,
+              'lor_gamma': gamma_list,
+              'lor_d_A': dA,
+              'lor_d_mean': dx0,
+              'lor_d_gamma': dgamma}
+
+        df_pk = pd.DataFrame(pk)
+        pk_file_path = f"data_non_confocal/clean_data/{title}_peaks.csv"
+        df_pk.to_csv(pk_file_path, index=False)
+        print(f"Peaks saved to {pk_file_path}")
--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.signal import find_peaks, peak_widths
+import matplotlib.pyplot as plt
 
 
 def crop_to_min_max(piezo_voltage, *readings):
@@ -20,7 +22,7 @@ def crop_to_min_max(piezo_voltage, *readings):
     # Ensure the possibility to analyse "backwards" sweeps too
     start_index = min(min_index, max_index)
     end_index = max(min_index, max_index)
-    
+
     pv = piezo_voltage[start_index:end_index + 1]
 
     if not (2 <= len(readings) <= 4):
@@ -53,3 +55,120 @@ def fit_piezo_line(time, piezo_voltage):
     print(f'slope = {slope} V/s\t intercept = {intercept} V')
 
     return piezo_fit
+
+
+def peaks(piezo_voltage, laser_voltage, height, distance, indices=False):
+    '''
+    Finds peaks in readings from the photodiode.
+
+    Parameters:
+
+    piezo_voltage: voltages on the piezo, should be the cleaned version (the output of fit_piezo_line)
+    laser_voltage: voltages on the photodiode
+    height: min height of the peaks
+    distance: min number of point between a peak and the following one
+    indices: bool to determine wheter to return peak indices as well
+
+    Returns: 
+
+    peaks_xvalues: voltage values on the piezo corresponding to detected peaks
+    peaks: detected peaks
+    scaled_widths: width of each peak in Volts
+    indices (if bool indices == True): indices of the peaks
+    '''
+    peaks_indices, _ = find_peaks(
+        laser_voltage, height=height, distance=distance)
+    peaks = laser_voltage[peaks_indices]
+    peaks_xvalues = piezo_voltage[peaks_indices]
+
+    widths = peak_widths(laser_voltage, peaks_indices, rel_height=0.5)
+    piezo_voltage_spacing = np.mean(np.diff(piezo_voltage))
+    scaled_widths = widths[0]*piezo_voltage_spacing
+
+    if indices:
+        return peaks_xvalues, peaks, scaled_widths, peaks_indices
+    else:
+        return peaks_xvalues, peaks, scaled_widths
+
+
+def scattering(x, y, x_label, y_label, title, file_name, save):
+    plt.figure(figsize=(12, 6))
+    plt.scatter(x, y, label='Data', color='green')
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
+    plt.xticks(rotation=45)
+    plt.grid()
+    plt.tight_layout()
+    if save:
+        plt.savefig(file_name)
+        plt.close()
+    else:
+        plt.show()
+
+
+def lin_quad_fits(x, y):
+    '''
+    Performs linear and quadratic fits and returns coefficients and errors.
+    '''
+    if (len(x)>3):
+        # Perform linear fit
+        # coeffs = [a, b] for y = ax + b
+        coeffs_1, V1 = np.polyfit(x, y, 1, cov=True)
+        d_coeffs_1 = [np.sqrt(V1[0, 0]), np.sqrt(V1[1, 1])]
+        # Perform quadratic fit
+        # coeffs = [a, b, c] for y = ax^2 + bx + c
+        coeffs_2, V2 = np.polyfit(x, y, 2, cov=True)
+        d_coeffs_2 = [np.sqrt(V2[0, 0]), np.sqrt(V2[1, 1]), np.sqrt(V2[2, 2])]
+        return coeffs_1, coeffs_2, d_coeffs_1, d_coeffs_2
+    else:
+        coeffs_1 = np.polyfit(x, y, 1)
+        coeffs_2 = np.polyfit(x, y, 2)
+        return coeffs_1, coeffs_2, [0,0], [0,0,0]
+
+
+def plot_fits(x, y, x_label, y_label, file_name, title=None, save=False):
+    '''
+    After performing linear and quadratic fits plots data and fit curves.
+    
+    Returns coefficients and errors for both fits.
+    '''
+    coeffs_1, coeffs_2, d_coeffs_1, d_coeffs_2 = lin_quad_fits(x, y)
+
+    # Create polynomial functions from the coefficients
+    linear_fit = np.poly1d(coeffs_1)
+    quadratic_fit = np.poly1d(coeffs_2)
+
+    # Generate x values for the fitted curve (same x range as the original data)
+    x_fit = np.linspace(min(x), max(x), 100)  # Smooth line for plotting
+    lin_fit = linear_fit(x_fit)  # Calculate the fitted line
+    # Calculate corresponding y values for the fitted curve
+    quad_fit = quadratic_fit(x_fit)
+
+    # Format the coefficients for display
+    lin_coeff_label = f"Linear Fit: y = ({coeffs_1[0]:.2e})x + ({coeffs_1[1]:.2e})"
+    quad_coeff_label = (
+        f"Quadratic Fit: y = ({coeffs_2[0]:.2e})x² + ({coeffs_2[1]:.2e})x + ({coeffs_2[2]:.2e})"
+    )
+
+    plt.figure(figsize=(12, 6))
+    plt.scatter(x, y, label='Data', color='green', marker='x', s=30)
+    plt.plot(x_fit, lin_fit, label=lin_coeff_label,
+             color='blue', linestyle='--')  # Plot the linear fit
+    plt.plot(x_fit, quad_fit, label=quad_coeff_label, color='red',
+             linestyle='--')  # Plot the quadratic fit
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    if title is not None:
+        plt.title(title)
+    plt.legend()
+    plt.xticks(rotation=45)
+    plt.grid()
+    plt.tight_layout()
+    if save:
+        plt.savefig(file_name)
+        plt.close()
+    else:
+        plt.show()
+    return coeffs_1, coeffs_2, d_coeffs_1, d_coeffs_2
