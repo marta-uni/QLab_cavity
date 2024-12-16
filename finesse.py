a6c19e1de7dcb1bfdb6ed83463cb48eafee3ff3f
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2024-12-16 17:40:31 +0100

    writing to file

--- finesse.py
+++ finesse.py
@@ -28,52 +28,53 @@ ind_range = 400
 finesse_list = []
 d_finesse_list = []
 
-for title, h in zip(title_list, min_heights):
-    data = pd.read_csv(f'data_non_confocal/clean_data/{title}.csv')
-    volt_piezo = data['volt_piezo'].to_numpy()
-    volt_laser = data['transmission'].to_numpy()
+with open("data_non_confocal/finesse.txt", "w") as file:
+    for title, h in zip(title_list, min_heights):
+        data = pd.read_csv(f'data_non_confocal/clean_data/{title}.csv')
+        volt_piezo = data['volt_piezo'].to_numpy()
+        volt_laser = data['transmission'].to_numpy()
 
-    print(title)
+        file.write(f'\nPeaks in {title}\n')
 
-    xpeaks, ypeaks, peak_widths = fn.peaks(volt_piezo, volt_laser, h, 400)
-    peak_widths = [max(pw, 0.001) for pw in peak_widths]
-    lor, cov = fp.fit_peaks2(volt_piezo, volt_laser, xpeaks, peak_widths, ind_range)
+        xpeaks, ypeaks, peak_widths = fn.peaks(volt_piezo, volt_laser, h, 400)
+        peak_widths = [max(pw, 0.001) for pw in peak_widths]
+        lor, cov = fp.fit_peaks2(volt_piezo, volt_laser, xpeaks, peak_widths, ind_range)
 
-    x0_list = []
-    A_list = []
-    gamma_list = []
-    dgamma = []
+        x0_list = []
+        A_list = []
+        gamma_list = []
+        dgamma = []
 
-    for popt, pcov in zip(lor, cov):
-        A_list.append(popt[0])
-        x0_list.append(popt[1])
-        gamma_list.append(popt[2])
-        dgamma.append(np.sqrt(pcov[2, 2]))
+        for popt, pcov in zip(lor, cov):
+            A_list.append(popt[0])
+            x0_list.append(popt[1])
+            gamma_list.append(popt[2])
+            dgamma.append(np.sqrt(pcov[2, 2]))
 
-    x0_list = np.array(x0_list)
-    A_list = np.array(A_list)
-    gamma_list = np.array(gamma_list)
-    dgamma = np.array(dgamma)
-    
-    for g, dg in zip(gamma_list, dgamma):
-        print(f'gamma = {g} +/- {dg}')
+        x0_list = np.array(x0_list)
+        A_list = np.array(A_list)
+        gamma_list = np.array(gamma_list)
+        dgamma = np.array(dgamma)
+        
+        for g, dg in zip(gamma_list, dgamma):
+            file.write(f'gamma = {g} +/- {dg}\n')
 
-    fp.plot_piezo_laser_fit2(volt_piezo, volt_laser, file_name=f'data_non_confocal/figures/finesse/piezo_peaks_{title}.png', A=A_list,
-                            x0=x0_list, gamma=gamma_list, xpeaks=xpeaks, ypeaks=ypeaks, ind_range=ind_range, save=True)
+        fp.plot_piezo_laser_fit2(volt_piezo, volt_laser, file_name=f'data_non_confocal/figures/finesse/piezo_peaks_{title}.png', A=A_list,
+                                x0=x0_list, gamma=gamma_list, xpeaks=xpeaks, ypeaks=ypeaks, ind_range=ind_range, save=True)
 
-    fin = fsr_volt/(2*gamma_list)
-    d_fin = fin * np.sqrt((d_fsr_volt / fsr_volt) **
-                          2 + (dgamma / gamma_list)**2)
+        fin = fsr_volt/(2*gamma_list)
+        d_fin = fin * np.sqrt((d_fsr_volt / fsr_volt) **
+                            2 + (dgamma / gamma_list)**2)
 
-    d_finesse_list.extend(d_fin)
-    finesse_list.extend(fin)
+        d_finesse_list.extend(d_fin)
+        finesse_list.extend(fin)
 
-finesse_list = np.array(finesse_list)
-d_finesse_list = np.array(d_finesse_list)
+    finesse_list = np.array(finesse_list)
+    d_finesse_list = np.array(d_finesse_list)
 
-weights = 1 / d_finesse_list**2
+    weights = 1 / d_finesse_list**2
 
-finesse = np.sum(weights * finesse_list) / np.sum(weights)
-d_finesse = np.sqrt(1 / np.sum(weights))
+    finesse = np.sum(weights * finesse_list) / np.sum(weights)
+    d_finesse = np.sqrt(1 / np.sum(weights))
 
-print(f'Finesse = {finesse} +/- {d_finesse}')
+    file.write(f'\nFinesse = {finesse} +/- {d_finesse}')
