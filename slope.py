358522d752eb60c3c2670e8e2d1cd9823dd643b7
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2025-01-29 11:15:59 +0100

    error11-18 calibration

--- slope.py
+++ slope.py
@@ -8,13 +8,14 @@ import piecewise_regression
 #####################################################################################################
 # section with file specific values
 
-title_list = [f'calibrated_error{str(i).zfill(5)}_filtered' for i in range(1, 5)]
+title_list = [f'calibrated_error{str(i).zfill(5)}_filtered' for i in range(11, 19)]
+title_list = title_list[:5] + title_list[6:]
 '''# excluding files with "throw out note" (8 to 10) and error5 file that has a bad error signal
 title_list = title_list[:4] + title_list[5:7] + title_list[10:]'''
 
 '''ind_range = [200, 200, 150, 100, 250, 200, 200,
              200, 200, 200, 200, 150, 200, 200]'''
-ind_range = [200, 200, 150, 100]
+ind_range = [200, 200, 200, 200, 200, 200, 200]
 min_height = 0.04
 min_distance = 1000
 
@@ -31,8 +32,8 @@ d_D = []
 '''mod_f = [50, 40, 30, 20, 10, 5, 40, 40, 40, 40, 40, 40, 40, 40]
 mod_ampl = [510, 310, 250, 150, 30, 8, 360, 300, 250, 200, 140, 110, 400, 450]'''
 
-mod_f = [50, 40, 30, 20]
-mod_ampl = [510, 310, 250, 150]
+mod_f = [40, 40, 40, 40, 40, 40, 40]
+mod_ampl = [360, 300, 250, 200, 140, 400, 450]
 
 for i, title in enumerate(title_list):
     print(title)
@@ -43,9 +44,9 @@ for i, title in enumerate(title_list):
     reflection = data['reflection'].to_numpy()
     transmission = data['transmission'].to_numpy()
 
-    '''if title == 'error00018':
+    if title == 'calibrated_error00018_filtered':
         min_height = 0.025
-        min_distance = 200'''
+        min_distance = 200
 
     peaks_indices, _ = find_peaks(
         data['transmission'], height=min_height, distance=min_distance)
@@ -124,6 +125,6 @@ df = pd.DataFrame({
     'mod_ampl': mod_ampl
 })
 
-filename = os.path.join(file_path, 'calib_slopes.csv')
+filename = os.path.join(file_path, 'calib_slopes_power.csv')
 df.to_csv(filename, index=False)
 
