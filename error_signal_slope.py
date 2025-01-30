ebe6e97149f9c70f4d77d0dee95289ac924e7b50
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2025-01-30 22:29:21 +0100

    bad estimate of beta

--- error_signal_slope.py
+++ error_signal_slope.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
-from scipy.optimize import curve_fit
+from scipy.special import jv
+from scipy.optimize import fsolve, curve_fit
+from functools import partial
 
 data_folder = 'data_non_confocal/clean_data'
 
@@ -42,35 +44,28 @@ for label, df in grouped_peaks.items():
     A_c.append(carrier)
     dA_c.append(d_carrier)
 
-# factor_list = [unp.sqrt(c * s) for c, s in zip(unp.uarray(A_s, dA_s), unp.uarray(A_c, dA_c))]
-
-factor_arr = unp.sqrt(unp.uarray(A_s, dA_s) * unp.uarray(A_c, dA_c))
-
 slope_data = pd.read_csv(f'{data_folder}/calib_slopes.csv')
 
 mod_f = slope_data['mod_f'].to_numpy()
-D_unc = unp.uarray(np.abs(slope_data['D'].to_numpy()), slope_data['d_D'].to_numpy())
 
-y_unc = D_unc / factor_arr
-
-y_fit = unp.nominal_values(y_unc)
-dy_fit = unp.std_devs(y_unc)
+D_values = np.abs(slope_data['D'].to_numpy())
+d_D = slope_data['d_D'].to_numpy()
 
 
 def linear(x, a, b):
     return a * x + b
 
 
-popt, pcov = curve_fit(linear, xdata=mod_f, ydata=y_fit, p0=[
-                       0, np.mean(y_fit)], sigma=dy_fit, absolute_sigma=True)
+popt, pcov = curve_fit(linear, xdata=mod_f, ydata=D_values, p0=[
+                       0, np.mean(D_values)], sigma=d_D, absolute_sigma=True)
 
-print(f'Fit result: a = {popt[0]} +/- {np.sqrt(pcov[0, 0])} MHz^-1')
-print(f'b = {popt[1]} +/- {np.sqrt(pcov[1, 1])}')
+print(f'Fit result: a = {popt[0]} +/- {np.sqrt(pcov[0, 0])} V*MHz^-2')
+print(f'b = {popt[1]} +/- {np.sqrt(pcov[1, 1])} V')
 
 xplot = np.linspace(mod_f[0], mod_f[-1], 100)
 
 plt.figure()
-plt.errorbar(mod_f, y_fit, dy_fit, ls='', label='Data', color='blue', fmt='.')
+plt.errorbar(mod_f, D_values, d_D, ls='', label='Data', color='blue', fmt='.')
 plt.plot(xplot, linear(xplot, *popt),
          label='Linear Fit', color='red', linewidth=2)
 plt.axvline(hwhm.n, color='black', label=f'2*HWHM: {hwhm} MHz')
@@ -78,9 +73,30 @@ y_min, y_max = plt.gca().get_ylim()
 plt.fill_betweenx(y=np.linspace(y_min, y_max, 100), x1=hwhm.n -
                   hwhm.std_dev, x2=hwhm.n + hwhm.std_dev, color='grey', alpha=0.3)
 plt.xlabel('Frequency modulation [MHz]')
-plt.ylabel('D/sqrt(AcAs) [MHz^-1]')
+plt.ylabel('D [V/MHz]')
 plt.legend()
 plt.grid()
 plt.tight_layout()
 plt.savefig('data_non_confocal/figures/error_signal_slope.png')
+
+As_Ac = unp.uarray(A_s, dA_s) / unp.uarray(A_c, dA_c)
+
+
+def extract_beta(x, asac):
+    return (jv(1, x))**2 / (jv(0, x))**2 - asac
+
+
+x = np.linspace(0, 2, 100)
+y = (jv(1, x))**2 / (jv(0, x))**2
+
+beta = [fsolve(partial(extract_beta, asac=ratio.nominal_value), 0.9)[0]
+        for ratio in As_Ac]
+
+plt.figure()
+plt.plot(mod_f, beta, color='red', linewidth=2, marker='o')
+plt.xlabel('Frequency modulation [MHz]')
+plt.ylabel(r'$\beta$')
+plt.grid()
+plt.tight_layout()
+plt.savefig('data_non_confocal/figures/beta_bad_estimate.png')
 plt.show()
